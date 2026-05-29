e2a52febd1eb06f47bf1a4bc1c74a68701ccfdb8
shiotakeishi <example.com>
2026-05-29 16:20:44 +0900

    アプリアイコンを組み込む

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(QStringLiteral(":/icons/app-icon-transparent.png")));
 
     MainWindow window;
     window.setWindowTitle(QStringLiteral("fileInfoChanger"));
+    window.setWindowIcon(QIcon(QStringLiteral(":/icons/app-icon-transparent.png")));
     window.show();
 
     return app.exec();
